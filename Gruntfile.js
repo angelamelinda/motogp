2bfbab79100753556187b914fa69cdfc2ad8d239
Angela <angela@softwareseni.com>
2018-08-17 01:24:41 +0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: { // will be used only during development
+            dist: {
+                files: {
+                    // destination // source file
+                    'css/style.css' : 'scss/style.scss'
+                }
+            }
+        },
+
+        cssmin: {
+            build: {
+                src: 'css/style.css',
+                dest: 'css/style.min.css'
+            }
+        },
+
+        uglify: {
+            build: {
+                files: {
+                    'js/app.min.js': ['js/app.js']
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: '**/*.scss',
+                tasks: ['css'],
+                options: {
+                    livereload: 35728
+                }
+            },
+
+            uglify: {
+                files: 'js/app.js',
+                tasks: ['uglify']
+            },
+
+            all: {
+                files: ['**/*.html'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    })
+
+    // Default task
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('css', ['sass', 'cssmin']);
+    grunt.registerTask('js', ['uglify']);
+
+    // Load up tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+}
